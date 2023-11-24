b39e896fea96d0799a2366c4c68281325974d8ae
Markos Beyene <markosbeyene1@gmail.com>
2023-11-24 16:41:35 +0300

    added a list <Genre > Genres and class Genre to store the id and name of the genre that the api retruns

--- CinemaSeatBooking/Models/MovieDetails.cs
+++ CinemaSeatBooking/Models/MovieDetails.cs
@@ -6,6 +6,7 @@
         public List<MovieResult>? results { get; set; }
         public int? total_pages { get; set; }
         public int? total_results { get; set; }
+        public List<Genre> Genres { get; set; }
     }
 
     public class MovieResult
@@ -23,7 +24,11 @@
         public string? title { get; set; }
         public bool? video { get; set; }
         public double? vote_average { get; set; }
-        public int? vote_count { get; set; }
+        public int? vote_count { get; set; } 
     }
-
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
 }
