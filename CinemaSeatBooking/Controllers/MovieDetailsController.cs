7ced56fc2bfd2030a47eef8369147e3aa7dcf0bd
Markos Beyene <markosbeyene1@gmail.com>
2023-11-24 10:37:29 +0300

    Added a page to display movie details

---
+++ CinemaSeatBooking/Controllers/MovieDetailsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CinemaSeatBooking.Models;
+using Newtonsoft.Json;
+using static Microsoft.AspNetCore.Razor.Language.TagHelperMetadata;
+
+namespace CinemaSeatBooking.Controllers
+{
+    public class MovieDetailsController : Controller
+    {
+        [HttpPost]
+        public IActionResult Details(string movieCode, string companyName, string overview, string posterUrl, string movieName)
+        {
+            // Your logic to handle the movie details
+            // You can use the received parameters to fetch additional data or perform any necessary operations.
+
+            // Redirect to the details page with the received parameters
+            return RedirectToAction("DetailsViewPage", new
+            {
+                movieCode = movieCode,
+                companyName = companyName,
+                overview = overview,
+                posterUrl = posterUrl,
+                movieName = movieName   
+            });
+        }
+
+        public IActionResult DetailsViewPage(string movieCode, string companyName, string overview, string posterUrl, string movieName)
+        {
+            // Your logic to fetch additional data or process the received values
+            var model = new MovieModel
+            {
+                MovieCode = movieCode,
+                CompanyName = companyName,
+                Overview = overview,
+                PosterUrl = posterUrl,
+                MovieName = movieName
+            };
+
+            return View(model);
+        }
+
+
+    }
+}
