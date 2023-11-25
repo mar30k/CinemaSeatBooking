89c750e62d25e0df4ec932c14891e7aedbcb60a4
Markos Beyene <markosbeyene1@gmail.com>
2023-11-25 12:33:54 +0300

    Added a functionality of passing the backdrop path the casts and genres to the detailsPage view

--- CinemaSeatBooking/Controllers/MovieDetailsController.cs
+++ CinemaSeatBooking/Controllers/MovieDetailsController.cs
@@ -1,15 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using CinemaSeatBooking.Models;
+﻿using CinemaSeatBooking.Models;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using static Microsoft.AspNetCore.Razor.Language.TagHelperMetadata;
+using Newtonsoft.Json.Linq;
 
 namespace CinemaSeatBooking.Controllers
 {
     public class MovieDetailsController : Controller
     {
+        private readonly string apiKey = "1ba83335ce22421020a77845254a578e";
+        private readonly string baseUrl = "https://api.themoviedb.org/3/movie/";
+
         [HttpPost]
         public IActionResult Details(string movieCode, string companyName, string overview,
-             string posterUrl, string movieName, int movieId, List<string> genre)
+             string posterUrl, string movieName, int movieId, string backdropPath)
         {
             return RedirectToAction("DetailsViewPage", new
             {
@@ -19,29 +22,86 @@ namespace CinemaSeatBooking.Controllers
                 posterUrl = posterUrl,
                 movieName = movieName,
                 movieId = movieId,
-                genre = genre
+                backdropPath = backdropPath
             });
         }
 
-        public IActionResult DetailsViewPage(string movieCode, string companyName, string overview,
-            string posterUrl, string movieName, int movieId, List<string> genre)
+        public async Task<IActionResult> DetailsViewPage(string movieCode, string companyName, string overview,
+        string posterUrl, string movieName, int movieId ,string backdropPath)
         {
-            // Your logic to fetch additional data or process the received values
-            var model = new MovieModel
+            // Construct the API URL
+            string apiUrl = $"{baseUrl}{movieId}?api_key={apiKey}&append_to_response=release_dates";
+
+            // Make the API call
+            using (HttpClient client = new HttpClient())
             {
-                MovieCode = movieCode,
-                CompanyName = companyName,
-                Overview = overview,
-                PosterUrl = posterUrl,
-                MovieName = movieName,
-                MovieId = movieId,
-                Genre = genre
-            };
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
 
-            return View(model);
+                if (response.IsSuccessStatusCode)
+                {
+                    // Read and parse the response content
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    var movieDetails = JsonConvert.DeserializeObject<MovieModel>(responseData);
+
+                    // Set additional properties in your MovieModel
+                    movieDetails.MovieCode = movieCode;
+                    movieDetails.CompanyName = companyName;
+                    movieDetails.Overview = overview;
+                    movieDetails.PosterUrl = posterUrl;
+                    movieDetails.MovieName = movieName;
+                    movieDetails.BackdropPath = backdropPath;
+                    // Extract genre information from movieDetails
+                    var genreNames = new List<string>();
+                    if (movieDetails.Genre != null)
+                    {
+                        foreach (var genreInfo in movieDetails.Genre)
+                        {
+                            if (genreInfo is string)
+                            {
+                                genreNames.Add((string)genreInfo);
+                            }
+                            else if (genreInfo is JObject)
+                            {
+                                string genreName = ((JObject)genreInfo)["name"].ToString();
+                                genreNames.Add(genreName);
+                            }
+                        }
+                    }
+
+                    // Assign the genre names to the GenreNames property in MovieModel
+                    movieDetails.GenreNames = genreNames;
+                    string castApiUrl = $"{baseUrl}{movieId}/credits?api_key={apiKey}";
+
+                    // Make the API call for cast details
+                    HttpResponseMessage castResponse = await client.GetAsync(castApiUrl);
+
+                    if (castResponse.IsSuccessStatusCode)
+                    {
+                        string castResponseData = await castResponse.Content.ReadAsStringAsync();
+                        var castDetails = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(castResponseData);
+
+                        // Extract cast names and profile paths directly
+                        var castList = ((IEnumerable<dynamic>)castDetails["cast"])
+                            .Select(c => new CastMember
+                            {
+                                Name = c["name"].ToString(),
+                                ProfilePath = "https://image.tmdb.org/t/p/w500"+c["profile_path"].ToString()
+                            })
+                            .ToList();
+
+                        // Assign the castList to the Cast property in MovieModel
+                        movieDetails.Cast = castList;
+                    }
+                    return View(movieDetails);
+                }
+                else
+                {
+                    // Handle API error
+                    return View("Error");
+                }
+            }
+
         }
-
 
     }
 }
-
