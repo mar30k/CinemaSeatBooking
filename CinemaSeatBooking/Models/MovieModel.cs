cc3f9d2717e195048f53dc362f110a77cb27cfcc
Markos Beyene <markosbeyene1@gmail.com>
2023-11-27 17:28:17 +0300

    added a movie schedule class for the normal and vip schedules

--- CinemaSeatBooking/Models/MovieModel.cs
+++ CinemaSeatBooking/Models/MovieModel.cs
@@ -3,7 +3,7 @@
     public class MovieModel
     {
         public string? MovieCode { get; set; }
-        public string? BackdropPath {  get; set; }
+        public string? BackdropPath { get; set; }
         public string? ArticleCode { get; set; }
         public string? MovieName { get; set; }
         public string? CompanyName { get; set; }
@@ -13,7 +13,7 @@
         public string? MoviePreviewImageUrl { get; set; }
         public int? ReleaseYear { get; set; }
         public string? CompanyTinNumber { get; set; }
-        public string? PosterUrl {  get; set; }
+        public string? PosterUrl { get; set; }
         public string? Overview { get; set; }
         public List<int>? GenreId { get; set; }
         public int? MovieId { get; set; }
@@ -22,7 +22,7 @@
         public List<string> GenreNames { get; set; }
         public List<CastMember> Cast { get; set; }
         public int? RunTime { get; set; }
-        public string? YoutubeKey {  get; set; }
+        public string? YoutubeKey { get; set; }
         public string FormattedRunTime
         {
             get
@@ -36,15 +36,46 @@
                 return null;
             }
         }
+
+        public List<MovieSchedule> Schedules { get; set; } // Added property for cinema schedules
     }
+
+    public class MovieSchedule
+    {
+        public string Code { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string SpaceCode { get; set; }
+        public string Dimension { get; set; }
+        public string HallName { get; set; }
+        public string Status { get; set; }
+        public string Header { get; set; }
+        public decimal NPrice { get; set; }
+        public VIPSchedule VIP { get; set; }
+    }
+
+    public class VIPSchedule
+    {
+        public string Code { get; set; }
+        public string Time { get; set; }
+        public string SpaceCode { get; set; }
+        public string HallName { get; set; }
+        public string Dimension { get; set; }
+        public string Header { get; set; }
+        public string Status { get; set; }
+        public decimal VPrice { get; set; }
+    }
+
     public class Genre
     {
         public int Id { get; set; }
         public string Name { get; set; }
     }
+
     public class CastMember
     {
         public string Name { get; set; }
         public string ProfilePath { get; set; }
     }
+
 }
