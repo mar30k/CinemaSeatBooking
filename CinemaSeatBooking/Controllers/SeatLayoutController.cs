cbc5fe996c9388f0cd64a46e4472ef5f390a9cfd
Markos Beyene <markosbeyene1@gmail.com>
2023-11-29 16:32:00 +0300

    passed movie schedule code to seatlayout controller form details page to do another api call to getbookedseats end point and retrive the response list for booked seats

--- CinemaSeatBooking/Controllers/SeatLayoutController.cs
+++ CinemaSeatBooking/Controllers/SeatLayoutController.cs
@@ -14,16 +14,17 @@ namespace CinemaSeatBooking.Controllers
             _httpClient.BaseAddress = new Uri("https://api-hulubeje.cnetcommerce.com/api/");
         }
         [HttpPost]
-        public IActionResult SeatArrangement(string spacecode, string companyTinNumber)
+        public IActionResult SeatArrangement(string spacecode, string companyTinNumber, string code)
         {
             return RedirectToAction("SeatArrangementView", new
             {
                 spacecode = spacecode,
                 companyTinNumber = companyTinNumber,
+                code = code,
             });
         }
 
-        public async Task<IActionResult> SeatArrangementView(string spacecode, string companyTinNumber)
+        public async Task<IActionResult> SeatArrangementView(string spacecode, string companyTinNumber, string code)
         {
             HttpResponseMessage response = await _httpClient.GetAsync($"cinema/getCinemaSeatArrangment?orgTin={companyTinNumber}&spaceCode={spacecode}");
 
@@ -34,12 +35,31 @@ namespace CinemaSeatBooking.Controllers
                 // Deserialize into a single SeatLayout instance
                 SeatLayout seatArrangement = JsonConvert.DeserializeObject<SeatLayout>(responseData);
 
-                // Pass the single SeatLayout instance to the view
+                // Make a second API call to get booked seats
+                HttpResponseMessage bookedSeatsResponse = await _httpClient.GetAsync($"cinema/GetBookedSeats?orgTin={companyTinNumber}&scheduleCode={code}&spaceCode={spacecode}");
+
+                if (bookedSeatsResponse.IsSuccessStatusCode)
+                {
+                    string bookedSeatsData = await bookedSeatsResponse.Content.ReadAsStringAsync();
+
+                    // Deserialize the response into a List<string>
+                    List<string> bookedSeats = JsonConvert.DeserializeObject<List<string>>(bookedSeatsData);
+
+                    // Update the SeatLayout model with the booked seats information
+                    seatArrangement.BookedSeats ??= bookedSeats;
+                }
+                else
+                {
+                    // Handle the case when the second API call fails
+                    return View("Error");
+                }
+
+                // Pass the updated SeatLayout instance to the view
                 return View(seatArrangement);
             }
 
-            // Handle the case when the response is not successful
-            return View("Error"); // You can create an "Error.cshtml" view for error handling
+            // Handle the case when the first API call fails
+            return View("Error");
         }
 
     }
