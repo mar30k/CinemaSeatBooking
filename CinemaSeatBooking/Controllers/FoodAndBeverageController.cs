6b06fc9a59fea60eaa8756dbd840c6ec7d82869b
Markos Beyene <markosbeyene1@gmail.com>
2023-12-05 17:08:51 +0300

    displayed the products (Food and beverage on products view)

---
+++ CinemaSeatBooking/Controllers/FoodAndBeverageController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CinemaSeatBooking.Models;
+namespace CinemaSeatBooking.Controllers
+{
+    public class FoodAndBeverageController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public FoodAndBeverageController()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://api-hulubeje.cnetcommerce.com/api/")
+            };
+        }
+        [HttpPost]
+        public IActionResult Products([FromForm] string selectedSeats, [FromForm] int countdownTime, [FromForm] string companyTinNumber)
+        {
+            // Convert the selectedSeats string to a list or array as needed
+            return RedirectToAction("ProductsView", new
+            {
+                countdownTime,
+                companyTinNumber,
+                selectedSeats,
+            });
+
+        }
+
+        public async Task<IActionResult> ProductsView(string companyTinNumber)
+        {
+
+            HttpResponseMessage response = await _httpClient.GetAsync($"Product/GetProducts?orgTin={companyTinNumber}&type=Restaurant&consignee=0912141914&platform=Web&longitude=0");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responsedata = await response.Content.ReadAsStringAsync();
+
+                var productsData = JsonConvert.DeserializeObject<List<Product>>(responsedata);
+
+                return View(productsData);
+            }
+            else
+            {
+                return View(null);
+            }
+
+        }
+    }
+
+}
---
+++ CinemaSeatBooking/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace CinemaSeatBooking.Models
+{
+    public class Product
+    {
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? ProductType { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+        public decimal? Price { get; set; }
+        public int? TaxType { get; set; }
+        public decimal? TaxValue { get; set; }
+        public List<string>? Pictures { get; set; }
+        public List<Product>? Children { get; set; }
+        public bool? IsInWishList { get; set; }
+        public int? StockInformation { get; set; }
+        public bool? HasPrescription { get; set; }
+        public double? Rating { get; set; }
+    }
+
+    public class RootProduct
+    {
+        public List<Product>? Products { get; set; }
+    }
+
+
+}
